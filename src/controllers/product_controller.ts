74a2731205a7b87c8a107289951f461f1f540556
firas <fkmkh11@gmail.com>
2024-09-13 23:24:08 +0300

    modify products entity and add brands table

--- src/controllers/product_controller.ts
+++ src/controllers/product_controller.ts
@@ -1,5 +1,6 @@
 import { Products } from "../entities/products/Products";
 import { Request, Response } from "express";
+import { Between } from "typeorm";
 
 
 
@@ -54,6 +55,138 @@ export const getProductInDetail = async (req: Request, res: Response) => {
             return res.status(404).json({ message: 'Product not found' });
         }
         res.status(200).send(product);
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error });
+    }
+}
+
+
+export const getProductsByCategory = async (req: Request, res: Response) => {
+    try {
+        const { category } = req.params;
+
+        const products = await Products.find({ where: { SubCategory: { Category: { Name: category } } } });
+        if (!products) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+
+        const productsList = products.map(product => {
+            let sum = 0;
+            return {
+                ProductID: product.ProductID,
+                ProductName: product.Name,
+                ProductPrice: product.Price,
+                ProductImage: product.Resource[0],
+                ProductSubCategory: product.SubCategory.Name,
+                ProductCategory: product.SubCategory.Category.Name,
+                ProductReview: product.Review.map(review => {
+                    sum += review.Rating;
+                    if (product.Review.length === 0) {
+                        return {
+                            avgRating: 0,
+                        }
+                    } else if (product.Review[product.Review.length - 1].ReviewID == review.ReviewID) {
+                        return {
+                            avgRating: sum / product.Review.length,
+                        }
+                    }
+                })
+            };
+        });
+
+        if (!productsList) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+        res.status(200).send(productsList);
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error });
+    }
+}
+
+export const getProducsBySubCategory = async (req: Request, res: Response) => {
+    try {
+        const { subCategory } = req.params;
+
+        const products = await Products.find({ where: { SubCategory: { Name: subCategory } } });
+        if (!products) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+
+        const productsList = products.map(product => {
+            let sum = 0;
+            return {
+                ProductID: product.ProductID,
+                ProductName: product.Name,
+                ProductPrice: product.Price,
+                ProductImage: product.Resource[0],
+                ProductSubCategory: product.SubCategory.Name,
+                ProductCategory: product.SubCategory.Category.Name,
+                ProductReview: product.Review.map(review => {
+                    sum += review.Rating;
+                    if (product.Review.length === 0) {
+                        return {
+                            avgRating: 0,
+                        }
+                    } else if (product.Review[product.Review.length - 1].ReviewID == review.ReviewID) {
+                        return {
+                            avgRating: sum / product.Review.length,
+                        }
+                    }
+                })
+            };
+        });
+
+        if (!productsList) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+        res.status(200).send(productsList);
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error });
+    }
+}
+
+
+export const getProductsByPriceRange = async (req: Request, res: Response) => {
+    try {
+        const { min, max } = req.params;
+
+
+        const products = await Products.find({
+            where: {
+                Price: Between(Number(min), Number(max))
+            }
+        });        if (!products) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+
+        const productsList = products.map(product => {
+            let sum = 0;
+            return {
+                ProductID: product.ProductID,
+                ProductName: product.Name,
+                ProductPrice: product.Price,
+                ProductImage: product.Resource[0],
+                ProductSubCategory: product.SubCategory.Name,
+                ProductCategory: product.SubCategory.Category.Name,
+                ProductReview: product.Review.map(review => {
+                    sum += review.Rating;
+                    if (product.Review.length === 0) {
+                        return {
+                            avgRating: 0,
+                        }
+                    } else if (product.Review[product.Review.length - 1].ReviewID == review.ReviewID) {
+                        return {
+                            avgRating: sum / product.Review.length,
+                        }
+                    }
+                })
+            };
+        });
+
+        if (!productsList) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+        res.status(200).send(productsList);
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error: error });
     }
--- src/entities/products/Products.ts
+++ src/entities/products/Products.ts
@@ -1,60 +1,78 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    CreateDateColumn,
-    UpdateDateColumn,
-    BaseEntity,
-    JoinColumn,
-    OneToMany
-  } from 'typeorm';
-  import { Resources } from '../Resources';
-  import { Offers } from '../Offers';
-  import { Reviews } from '../Reviews';
-  import { SubCategories } from '../categories/SubCategories';
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+  JoinColumn,
+  OneToMany,
+  OneToOne
+} from 'typeorm';
+import { Resources } from '../Resources';
+import { Offers } from '../Offers';
+import { Reviews } from '../Reviews';
+import { SubCategories } from '../categories/SubCategories';
 import { ProductCustomizations } from './ProductCustomizations';
-  
-  @Entity()
-  export class Products extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    ProductID: number;
-  
-    @Column('varchar')
-    Name: string;
-  
-    @Column('text')
-    Description: string;
-  
-    @Column('decimal')
-    Price: number;
-  
-    @Column('int')
-    Quantity: number;
-  
-    @Column('text')
-    Message: string;
+import { Brands } from '../Brands';
 
-    @OneToMany(()=> ProductCustomizations, (productCustomization) => productCustomization.ProductCustomizationID)
-    @JoinColumn({ name: 'ProductCustomizationID' })
-    ProductCustomization: ProductCustomizations[];
-  
-    @OneToMany(() => Resources, (resource) => resource.ResourceID)
-    Resource: Resources[];
-  
-    @ManyToOne(() => Offers, (offer) => offer.OfferID)
-    Offer: Offers;
-  
-    @OneToMany(() => Reviews, (review) => review.ReviewID)
-    Review: Reviews[];
-  
-    @ManyToOne(() => SubCategories, (subcategory) => subcategory.SubCategoryID)
-    SubCategory: SubCategories;
-  
-    @CreateDateColumn()
-    CreatedAt: Date;
-  
-    @UpdateDateColumn()
-    UpdatedAt: Date;
-  }
-  
+@Entity()
+export class Products extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  ProductID: number;
+
+  @Column('varchar')
+  Name: string;
+
+  @Column('text')
+  Description: string;
+
+  @Column('decimal')
+  Price: number;
+
+  @Column('int')
+  Quantity: number;
+
+  @Column('text')
+  Message: string;
+
+  @Column('enum', {
+    enum: ['None',
+      'cotton', 'polyester', 'nylon', 'silk', 'wool', 'leather', 'rubber', 'linen',
+      'denim', 'cashmere', 'velvet', 'satin', 'suede', 'tweed', 'corduroy', 'chiffon',
+      'georgette', 'muslin', 'organza', 'taffeta', 'velour', 'velveteen', 'viscose',
+      'acrylic', 'rayon', 'spandex', 'lycra', 'modal', 'bamboo', 'jute', 'hemp', 'ramie',
+      'acetate', 'acrylic', 'lyocell', 'modacrylic', 'olefin', 'polypropylene', 'elastane',
+    ], default: 'None'
+  })
+  Material: string;
+
+
+  @OneToOne(() => Brands, (brand) => brand.BrandID)
+  @JoinColumn({ name: 'BrandID' })
+  Brand: Brands;
+
+
+  @OneToMany(() => ProductCustomizations, (productCustomization) => productCustomization.ProductCustomizationID)
+  @JoinColumn({ name: 'ProductCustomizationID' })
+  ProductCustomization: ProductCustomizations[];
+
+  @OneToMany(() => Resources, (resource) => resource.ResourceID)
+  Resource: Resources[];
+
+  @ManyToOne(() => Offers, (offer) => offer.OfferID)
+  Offer: Offers;
+
+  @OneToMany(() => Reviews, (review) => review.ReviewID)
+  Review: Reviews[];
+
+  @ManyToOne(() => SubCategories, (subcategory) => subcategory.SubCategoryID)
+  SubCategory: SubCategories;
+
+  @CreateDateColumn()
+  CreatedAt: Date;
+
+  @UpdateDateColumn()
+  UpdatedAt: Date;
+}
