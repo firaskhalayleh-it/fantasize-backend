85664f56ccfb49734b64151ca5c6830c1d1d4986
firas <fkmkh11@gmail.com>
2024-09-22 21:03:18 +0300

    modify brands entity

--- src/entities/Brands.ts
+++ src/entities/Brands.ts
@@ -1,17 +1,18 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { Products } from '../entities/products/Products';
 
+export const BrandName = ['Nike', 'Adidas', 'Puma', 'Reebok', 'Under Armour', 'ZARA', 'H&M', 'Levi Strauss & Co.', 'Gucci',
+    'Prada', 'Chanel', 'Dior', 'Versace', 'Armani', 'Calvin Klein', 'Tommy Hilfiger', 'Ralph Lauren',
+    'Hugo Boss', 'Lacoste', 'Burberry', 'Fendi', 'Balenciaga', 'Givenchy', 'Valentino', 'Dolce & Gabbana',
+    'Yves Saint Laurent', 'Alexander McQueen', 'Vetements', 'Off-White', 'Balmain', 'None'
+];
 @Entity()
 export class Brands extends BaseEntity {
     @PrimaryGeneratedColumn('increment')
     BrandID: number;
 
     @Column('enum', {
-        enum: ['Nike', 'Adidas', 'Puma', 'Reebok', 'Under Armour', 'ZARA', 'H&M', 'Levi Strauss & Co.', 'Gucci',
-            'Prada', 'Chanel', 'Dior', 'Versace', 'Armani', 'Calvin Klein', 'Tommy Hilfiger', 'Ralph Lauren',
-            'Hugo Boss', 'Lacoste', 'Burberry', 'Fendi', 'Balenciaga', 'Givenchy', 'Valentino', 'Dolce & Gabbana',
-            'Yves Saint Laurent', 'Alexander McQueen', 'Vetements', 'Off-White', 'Balmain', 'None'
-        ],
+        enum: BrandName,
         default: 'None'
     })
     Name: string;
