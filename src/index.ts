b40d6dbb324cd31d2ed30048c7c59520a66f923b
yahyathalji <yahyajaradatssss@gmail.com>
2024-09-24 16:25:28 +0300

    i create file httpErrors.ts in middleware and using in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from "express";
 import { initializeDB } from "./config/database"; // التأكد من الاسم الصحيح للدالة
 import 'dotenv/config';
 import authRoute from "./routes/Auth Routes/authRoutes";
+import { errorHandler, notFound } from "./middlewares/httpErrors";
 
 const app = express();
 const PORT = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.use("/api",authRoute);
 app.listen(PORT, async () => {
