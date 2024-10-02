5058c51037b0679a28481bc63181fffa9904dd12
yahyathalji <yahyajaradatssss@gmail.com>
2024-10-02 19:35:50 +0300

    service remove a package from favorites

--- src/services/Packages Services/favoritePackagesServices.ts
+++ src/services/Packages Services/favoritePackagesServices.ts
@@ -49,7 +49,19 @@ try{
 //----------------------- Remove a package from favorites-----------------------
 export const s_removePackageFavorites = async (req:Request , res:Response) =>{
 try{
+        const packageId :any  = req.params.packageId;
+        const  userId  = (req as any).user.payload.userId;
+        const packageIsExist = await Packages.findOne({where:{PackageID:packageId}});
+        if(!packageIsExist){
+                return ({ message: "paackage not found" });
+        }
+        const favoritePackage = await FavoritePackages.findOne({where :{Package :{PackageID:packageId} , User:{UserID:userId}}})
+        if(!favoritePackage){
+            return ({ message: "Package not in favorites" });
+        };
+        await favoritePackage.remove();
 
+        return "package removed from favorites";
 }catch (err: any) {
         console.log(err);
         res.status(500).send({ message: err.message })
