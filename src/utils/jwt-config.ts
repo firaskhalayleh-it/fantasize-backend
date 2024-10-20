402db2e2b1faeedee215414025fc81f4add08eff
firas <fkmkh11@gmail.com>
2024-10-20 23:49:13 +0300

    Refactor jwt-config.ts and push-sender.ts: Remove unused code and comments

--- src/utils/jwt-config.ts
+++ src/utils/jwt-config.ts
@@ -2,24 +2,18 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import { Users } from '../entities/users/Users';
 
-
 const generateToken = async (userid: string) => {
-    // Fetch the user along with the related entity (e.g., UserProfilePicture)
     const user = await Users.findOne({
         where: { UserID: userid },
-        relations: ['UserProfilePicture', 'Role'],  // Make sure UserProfilePicture is fetched as a relation
+        relations: ['UserProfilePicture', 'Role'],  
     });
 
-
-    // Check if the user exists and UserProfilePicture contains entityName
     if (!user) {
         throw new Error('User not found');
     }
 
-    // If UserProfilePicture or entityName does not exist, handle it appropriately
     const entityName = user.UserProfilePicture ? user.UserProfilePicture.entityName : 'Unknown';
 
-    // Generate the JWT token with all required fields
     return jwt.sign(
         {
             payload: {
@@ -32,18 +26,16 @@ const generateToken = async (userid: string) => {
                     entityName: entityName,
                     filePath: user.UserProfilePicture.filePath,
                     fileType: user.UserProfilePicture.fileType
-                } : null, // Include entityName in the payload
+                } : null,
             }
         },
-        process.env.JWT_SECRET || 'secret', // Use secret from .env or fallback to 'secret'
-        { expiresIn: '12h' } // Token expires in 12 hours
+        process.env.JWT_SECRET || 'secret',
+        { expiresIn: '12h' }
     );
 };
 
 const verifyToken = (token: string) => {
-
     return jwt.verify(token, process.env.JWT_SECRET || 'secret');
 }
-
 
 export { generateToken, verifyToken }
